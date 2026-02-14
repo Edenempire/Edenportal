8ce96d33647620461699f5797242a75bd5ecd24e
parablesandplatters <philanikemele23@gmail.com>
2026-02-14 10:11:13 +0200

    all

--- sw.js
+++ sw.js
@@ -3,33 +3,39 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/logo-192.png',
-  '/logo-512.png',
-  '/style.css', // if you separate CSS
-  '/script.js'  // if you separate JS
+  '/logo.png',
+  '/logo.png',
+  '/logo.png',
+  '/style.css',  // if any external CSS
+  '/script.js',  // if you separate JS
+  '/images/hoss.png',
+  '/images/joy.jpg',
+  '/images/frost.jpg',
+  '/images/xan.jpg',
+  '/images/morpheus.jpg',
+  '/images/.jpg', // double-check this filename
+  '/images/parables.png',
+  '/images/hopco.jpg'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keyList) => Promise.all(
-      keyList.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })
-    ))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
